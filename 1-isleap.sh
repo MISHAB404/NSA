91593f9d9c34f018a080aa0665d6244355ae29b1
Mishab <113359139+MISHAB404@users.noreply.github.com>
2023-06-20 14:33:08 +0530

    Add files via upload

---
+++ 1-isleap.sh
@@ -0,0 +1,24 @@
+# find the leap year
+
+if [ $# -gt 1 ]
+then
+  echo "Syntax Error"
+  exit 1
+fi 
+if [ $# -eq 0 ]
+then 
+  echo "Enter a year : "
+  read year
+else
+  year=$1
+fi
+a=`expr $year % 4`
+b=`expr $year % 100`
+c=`expr $year % 400`
+
+if [ $a -eq 0 -a $b -ne 0 -o $c -eq 0 ]
+then 
+  echo "$year is a leap year"
+else
+  echo "$year is not a leap year"
+fi
